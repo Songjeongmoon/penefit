9caf6c0c9c97d53eafaea6d58bdb20db77d744a4
Songjeongmoon <wjdans5692@gmail.com>
2023-02-07 16:20:06 +0900

    3st commit

---
+++ penefit/src/main/java/com/penefit/moons/controller/ControllerAboutClassInfo.java
@@ -0,0 +1,15 @@
+package com.penefit.moons.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerAboutClassInfo {
+
+	
+	@GetMapping("/class/suggestion")
+	public void classSuggest() {
+	}
+	
+	
+}
